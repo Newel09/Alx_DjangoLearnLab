d9c94c665af6de14a89c5196597ca0dc330c9036
newel <tsogbenewel@gmail.com>
2026-01-18 13:58:53 +0000

    django-models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('member/', views.member_view, name='member_view'),
     
     # Permission-based views for Book operations
-    path('book/add/', views.add_book, name='add_book'),
-    path('book/<int:pk>/edit/', views.edit_book, name='edit_book'),
-    path('book/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]

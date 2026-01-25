8741b173984172d5849b1721ea8ec676d9bded01
newel <tsogbenewel@gmail.com>
2026-01-25 13:22:10 +0000

    django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    """Custom User model extending Django's AbstractUser with additional fields."""
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    
+    def __str__(self):
+        return self.username
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)

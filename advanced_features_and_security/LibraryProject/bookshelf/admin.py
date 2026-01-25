9de666748001e20f18b52b08073c41c688a8f8cc
newel <tsogbenewel@gmail.com>
2026-01-25 13:29:06 +0000

    django

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    """Custom admin for CustomUser model."""
+    list_display = ("email", "username", "date_of_birth", "is_staff", "is_superuser")
+    list_filter = ("is_staff", "is_superuser", "date_of_birth")
+    search_fields = ("email", "username")
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "password1", "password2"),
+        }),
+    )
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -7,4 +28,5 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ("author", "publication_year")
     search_fields = ("title", "author")
 
-# Register your models here.
+
+admin.site.register(CustomUser, CustomUserAdmin)
